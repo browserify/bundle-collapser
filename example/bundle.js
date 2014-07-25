0c1994d5a8cb11cca86b689497dc67ca504cb8c3
James Halliday <mail@substack.net>
2014-07-24 18:17:30 -0700

    example

---
+++ example/bundle.js
@@ -0,0 +1,9 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+done(require('./one'), require('./two'));
+},{"./one":2,"./two":3}],2:[function(require,module,exports){
+module.exports = 1;
+
+},{}],3:[function(require,module,exports){
+module.exports = 2;
+
+},{}]},{},[1]);
---
+++ readme.markdown
@@ -0,0 +1,5 @@
+# bundle-path-collapser
+
+Convert bundle paths to IDs to save bytes in browserify bundles.
+
+
