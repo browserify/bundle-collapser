1a28710d72a1ed0171a648940b3b3603e80dd586
James Halliday <mail@substack.net>
2014-07-24 18:40:05 -0700

    it works

--- example/collapse.js
+++ example/collapse.js
@@ -2,4 +2,4 @@ var collapse = require('../');
 var fs = require('fs');
 
 var src = fs.readFileSync(__dirname + '/bundle.js', 'utf8');
-console.log(collapse(src));
+collapse(src).pipe(process.stdout);
--- index.js
+++ index.js
@@ -1,2 +1,41 @@
+var unpack = require('browser-unpack');
+var pack = require('browser-pack');
+var falafel = require('falafel');
+
 module.exports = function (src) {
+    var rows = unpack(src);
+    var p = pack({ raw: true });
+    rows.forEach(function (row) {
+        row.source = replace(row.source, row.deps);
+        row.deps = {};
+        p.write(row);
+    });
+    p.end();
+    return p;
 };
+
+function replace (src, deps) {
+    return falafel(src, function (node) {
+        if (isRequire(node)) {
+            var value = node.arguments[0].value;
+            if (has(deps, value)) {
+                node.update('require(' + deps[value] + ')');
+            }
+        }
+    }).toString();
+}
+
+function isRequire (node) {
+    var c = node.callee;
+    return c
+        && node.type === 'CallExpression'
+        && c.type === 'Identifier'
+        && c.name === 'require'
+        && node.arguments[0]
+        && node.arguments[0].type === 'Literal'
+    ;
+}
+
+function has (obj, key) {
+    return Object.prototype.hasOwnProperty.call(obj, key);
+}
