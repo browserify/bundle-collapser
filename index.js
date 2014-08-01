ca6b4467361541f43ed7a4d348f770b2fa055c07
James Halliday <mail@substack.net>
2014-08-01 12:35:39 +1200

    reorganized

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var unpack = require('browser-unpack');
 var pack = require('browser-pack');
-var falafel = require('falafel');
+var replace = require('./lib/replace.js');
 
 module.exports = function (src) {
     var rows = unpack(src);
@@ -13,29 +13,3 @@ module.exports = function (src) {
     p.end();
     return p;
 };
-
-function replace (src, deps) {
-    return falafel(src, function (node) {
-        if (isRequire(node)) {
-            var value = node.arguments[0].value;
-            if (has(deps, value)) {
-                node.update('require(' + deps[value] + ')');
-            }
-        }
-    }).toString();
-}
-
-function isRequire (node) {
-    var c = node.callee;
-    return c
-        && node.type === 'CallExpression'
-        && c.type === 'Identifier'
-        && c.name === 'require'
-        && node.arguments[0]
-        && node.arguments[0].type === 'Literal'
-    ;
-}
-
-function has (obj, key) {
-    return Object.prototype.hasOwnProperty.call(obj, key);
-}
