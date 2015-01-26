c847b5879b84984e37a63ece7fd0062fd46c6ee1
Malte Wessel <malte-wessel@users.noreply.github.com>
2015-01-26 20:53:10 +0100

    Preserve dependency indexes. This will make bundle-collapser work with factor-bundle

--- plugin.js
+++ plugin.js
@@ -4,7 +4,11 @@ var replace = require('./lib/replace.js');
 module.exports = function apply (b, opts) {
     b.pipeline.get('label').push(through.obj(function (row, enc, next) {
         row.source = replace(row.source, row.deps);
-        row.deps = {};
+        var deps = {};
+		for (var key in row.deps) {
+			deps[row.deps[key]] = row.deps[key];
+		}
+        row.deps = deps;
         this.push(row);
         next();
     }));
