69b53a96d764ed2af721ba312d42593d52e91121
Ivan Nikolić <niksy5@gmail.com>
2015-09-06 11:31:27 +0200

    Add support for custom replacements

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var unpack = require('browser-unpack');
 var pack = require('browser-pack');
 var replace = require('./lib/replace.js');
 
-module.exports = function (src) {
+module.exports = function (src, opts) {
     var rows = unpack(src);
     var p = pack({ raw: true });
     rows.forEach(function (row) {
-        row.source = replace(row.source, row.deps);
+        row.source = replace(row.source, row.deps, opts && opts.custom);
         row.deps = {};
         p.write(row);
     });
